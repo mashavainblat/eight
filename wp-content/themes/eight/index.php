7c286008cdd7d45e041fe1bc0413ddce2deecef8
Masha Vainblat <MashaVainblat@Mashas-MacBook-Pro.local>
2016-04-25 13:39:44 -0400

    styling navigation to be responsive

--- wp-content/themes/eight/index.php
+++ wp-content/themes/eight/index.php
@@ -1,37 +1,44 @@
-<!-- cakepop project show page -->
+<!---->
+<!-- CAKEPOP PROJECT SHOW PAGE -->
+<!---->
 
 <?php get_header(); ?> <!-- <head></head> -->
 <body id="index">
 	<header>
 		<?php include ("navigation.php"); ?>
 	</header>
-	<div id="main-content" class="main-content">
-			<?php 
-				if ( have_posts() ) : while ( have_posts() ) : the_post();
-					// print_r($post);
-			?>
-			<h1><?php echo the_title(); ?> </h1> 
-			<p class="prevPost">
-				<?php
-					echo previous_post_link('%link','%title', true );
-				?>
-			</p>
-
-			
-			<p class="nextPost">
+    <div class="col-md-1 emptyColumn"></div>
+    <div class="col-md-10">
+		<div id="main-content" class="main-content">
 				<?php 
-					echo next_post_link('%link','%title', true );
+					if ( have_posts() ) : while ( have_posts() ) : the_post();
+						// print_r($post);
 				?>
-			</p>
+
+				<h1><?php echo the_title(); ?> </h1> 
+
+				<p class="prevPost">
+					<?php
+						echo previous_post_link('%link','prev', true );
+					?>
+				</p>
+				
+				<p class="nextPost">
+					<?php 
+						echo next_post_link('%link','next', true );
+					?>
+				</p>
 
 
-		<?php 
-			echo the_content();
-		//echo get_a_title(59);
-		endwhile; else : ?>
-		<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-		<?php endif; ?>
-	</div><!-- #main-content -->
+			<?php 
+				echo the_content();
+			//echo get_a_title(59);
+			endwhile; else : ?>
+			<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+			<?php endif; ?>
+		</div><!-- #main-content -->
+	</div>
+    <div class="col-md-1 emptyColumn"></div>
 
 	<footer>
 		<?php get_footer();
